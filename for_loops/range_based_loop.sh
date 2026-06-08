73071baabf15a5a6f5ebf586432299998a794017
Konda Balaji Rao <balajikonda9046@gmail.com>
2026-06-09 02:51:51 +0900

    updated syntax of for loop

--- for_loops/range_based_loop.sh
+++ for_loops/range_based_loop.sh
@@ -4,6 +4,8 @@ echo "Range Based For Loop"
 echo " "
 echo "in ths We define the starting, ending, and increment at every iteration in the statement"
 
+# for i in {start .. end .. increment};
+
 for i in {1..10}; 
 do 
     echo "$i"
