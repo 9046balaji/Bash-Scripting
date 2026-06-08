38d55a50a69bf5f94672a30d80dca3c358038cb3
Konda Balaji Rao <balajikonda9046@gmail.com>
2026-06-09 02:49:56 +0900

    range based for loop it has start, end and increment values it starts at starting value for iteration and prints that value and kepps on printing according to the increment

---
+++ for_loops/range_based_loop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Range Based For Loop"
+echo " "
+echo "in ths We define the starting, ending, and increment at every iteration in the statement"
+
+for i in {1..10}; 
+do 
+    echo "$i"
+done
