b8fd2a99d7000c8b1853ddeabfd0f22c23f89597
Konda Balaji Rao <balajikonda9046@gmail.com>
2026-06-09 02:38:13 +0900

    working of single and double quotes

---
+++ variables/single_double_quotes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "showing the differance between the single and double quotes"
+
+var="Balaji"
+
+echo "single quotes"
+
+echo '$var'
+echo "----------------------"
+
+
+echo "double quotes"
+echo "$var"
+
