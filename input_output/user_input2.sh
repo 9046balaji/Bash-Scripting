5cc0d323da6ef997bdb6a0cf02427414e204e5c1
Konda Balaji Rao <balajikonda9046@gmail.com>
2026-06-09 13:30:55 +0900

    using read -p to take input from user with printing a message

---
+++ input_output/user_input2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo "reading input from user usinf read -p insted of saprate commands"
+echo "we can use single (-p flag) to display message first and then take user input"
+echo " "
+
+read -p " what is your name " name
+
+echo "hi my name is $name "
