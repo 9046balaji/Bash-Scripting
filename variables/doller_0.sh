65c07a563b090379ac703b6f1a9d072456389b26
Konda Balaji Rao <balajikonda9046@gmail.com>
2026-06-09 14:22:04 +0900

    doller 0 is used to self distruct or delete the script after execution or display the name of the script file

---
+++ variables/doller_0.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "$ 0 is used to referance the script itself"
+echo "this is used to print the name of the script running like that"
+echo "if needed we can delete the script or self distruct after execution completed"
+
+echo " "
+
+echo "file name: $0"
+
+# rm -f $0 if needed to delete
