834111e42ba51254dc825a560f4e606c5a03630c
Konda Balaji Rao <balajikonda9046@gmail.com>
2026-06-11 12:55:18 +0900

    testing bash -x debug command

---
+++ Debugging/bash-x.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "testing the command bash -x "
+echo "it prints the commands before running them"
+echo "it is used to find the errors when executing the script"
+
+echo " "
+
+name="Balaji"
+echo "Hello $name"
